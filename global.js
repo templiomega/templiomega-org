35ede50280f8ee94a7a0382f625671eb23e461ac
templiomega <templiomega@gmail.com>
2025-08-25 00:09:15 -0400

    Update global.js

--- global.js
+++ global.js
@@ -1,5 +1,20 @@
 const menu = document.getElementById("menu");
 const navLinks = document.getElementById("navLinks");
+const gospelImg = document.getElementById("gospelImg");
+
+let imgInt = 0;
+
+const gospelImages = [
+
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg"
+
+]
 
 menu.addEventListener("click", menuCheck);
 
@@ -23,8 +38,27 @@ function menuCheck() {
 }
 
 
+function changeGospelImg() {
+
+    gospelImg.src = gospelImages[imgInt];
+    imgInt++;
+
+    if (imgInt > gospelImages.length - 1) {
+
+	imgInt = 0;
+
+    }
+
+    setTimeout(changeGospelImg, 3000);
+
+}
+
+
 function goHome() {
 
     window.location = "./";
     
 }
+
+
+setTimeout(changeGospelImg, 3000);
